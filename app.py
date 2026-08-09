6ba6e2ba64565c7d8c820c35c8ac623c8fb87d42
lopesmiranda15 <brunolopesmiranda15@gmail.com>
2026-08-09 16:21:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 import streamlit as st
 import gspread
-import os
 from google.oauth2.service_account import Credentials
 
 st.title("Conexão")
 
-# LISTA O QUE ELE ENCONTRA NA PASTA PARA VOCÊ VER
-st.write("Arquivos na pasta:", os.listdir('.'))
-
-if os.path.exists("credenciais.json"):
-    creds = Credentials.from_service_account_file(
-        "credenciais.json",
-        scopes=["https://www.googleapis.com/auth/spreadsheets"]
-    )
-    client = gspread.authorize(creds)
-    st.success("CONECTADO!")
-else:
-    st.error("ARQUIVO NAO ENCONTRADO")
+# Usando o nome exato que apareceu na tela com o ponto final
+creds = Credentials.from_service_account_file(
+    "credenciais.json.",
+    scopes=["https://www.googleapis.com/auth/spreadsheets"]
+)
+client = gspread.authorize(creds)
+st.success("CONECTADO COM SUCESSO!")
